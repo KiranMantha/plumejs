d73a7b582bf95cd8e86be05e40a9a277d1cc4747
KiranMantha <Kiran.ManthaVenkataS@rci.rogers.com>
2019-07-11 13:55:29 +0530

    removed unused types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,18 +1,6 @@
-type Type<T> = {
-  new (...args: any[]): T;
-};
-
 type DecoratorOptions = {
   selector: string
 }
-
-type Token<T> = Type<T>;
-
-type Dictionary = {
-  [key: string]: any;
-};
-
-type GenericClassDecorator<T> = (target: T) => void;
 
 type RouteItem = {
 	Params: any;
