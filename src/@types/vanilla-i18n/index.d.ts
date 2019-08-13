2571b92d7724cb8d8a32f554c0d7e101da9e7e81
KiranMantha <Kiran.ManthaVenkataS@rci.rogers.com>
2019-08-13 16:22:39 +0530

    changed typings

--- src/@types/vanilla-i18n/index.d.ts
+++ src/@types/vanilla-i18n/index.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="node" />
 // will be removed once watchjs has @types
 declare module 'vanilla-i18n' {
-  import watchjs from "vanilla-i18n";
   export function setDefaultLanguage(language:string):void;
   export function setTranslate(obj:any, language:string):void;
 }
