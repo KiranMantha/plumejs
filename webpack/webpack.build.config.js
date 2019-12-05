6e33347642f624bad25f4f815a30105e9fc336c6
KiranMantha <kiran.manthavenkatas@rci.rogers.com>
2019-12-05 11:04:55 +0530

    added bundle analyzer to check dependency sizes

--- webpack/webpack.build.config.js
+++ webpack/webpack.build.config.js
@@ -1,5 +1,8 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+baseConfig.plugins.push(new BundleAnalyzerPlugin());
 
 module.exports = merge(baseConfig, {
     
