ea95de70b04b9ca94911d61fae346e468dd881ba
KiranMantha <kiran.manthavenkatas@rci.rogers.com>
2019-12-09 13:40:40 +0530

    fixed typings

--- @types/typings.d.ts
+++ @types/typings.d.ts
@@ -1,7 +1,7 @@
 interface HTMLElement {
 	trigger: (eventName: string, isBubbling?: boolean) => void;
 	update: () => void;
-	getModel: () => Object;
+	getModel: () => { [key: string]: any };
 }
 
 interface Window {
