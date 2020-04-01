8a579f61107e284203e33738ac7bec89419d1030
KiranMantha <kiran.manthavenkatas@rci.rogers.com>
2020-04-01 16:22:50 +0530

    updated demo

--- docs/3.chunk.js
+++ docs/3.chunk.js
@@ -1,8 +1,8 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{43:function(e,t,s){"use strict";s.r(t);var r=s(0),o=s(1);let i=class PersonService{getPersons(){return fetch("https://jsonplaceholder.typicode.com/users").then(e=>e.json())}};i=Object(r.__decorate)([Object(o.Injectable)()],i);let n=class PersonsList{constructor(e,t){this.personSrvc=e,this.router=t,this.data=[],this.persondetails={},console.log("current route ",this.router.getCurrentRoute())}mount(){this.personSrvc.getPersons().then(e=>{this.data=e,this.update()})}alertName(e){this.persondetails=e,this.update()}render(){return o.html`
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{43:function(e,t,s){"use strict";s.r(t);var r=s(0),o=s(1);let i=class PersonService{getPersons(){return fetch("https://jsonplaceholder.typicode.com/users").then(e=>e.json())}};i=Object(r.__decorate)([Object(o.Injectable)()],i);let a=class PersonsList{constructor(e,t){this.personSrvc=e,this.router=t,this.data=[],this.persondetails={},console.log("current route ",this.router.getCurrentRoute())}mount(){this.personSrvc.getPersons().then(e=>{this.data=e,this.update()})}alertName(e){this.persondetails=e,this.update()}render(){return o.html`
 			<h4>Sample service injection with http call and passing data to other component</h4>
 			Current route data: <code>${JSON.stringify(this.router.getCurrentRoute(),null,2)}</code>
 			<div>
-				<ul class="list-group">
+				<ul class=" test list-group">
 					${this.data.map(e=>o.html`
 								<li class="list-group-item"
 									onclick=${()=>{this.alertName(e)}}
@@ -16,8 +16,8 @@
 					userDetails=${this.persondetails}
 				></person-details>
 			</div>
-		`}};n=Object(r.__decorate)([Object(o.Component)({selector:"persons-list",styleUrl:"persons/persons-list.scss"}),Object(r.__metadata)("design:paramtypes",[i,o.Router])],n);let a=class PersonDetails{constructor(){this.userDetails={}}render(){return console.log("selected: user",this.userDetails),this.userDetails.name?o.html`
+		`}};a=Object(r.__decorate)([Object(o.Component)({selector:"persons-list",styleUrl:"persons-list.scss",useShadow:!1}),Object(r.__metadata)("design:paramtypes",[i,o.Router])],a);let n=class PersonDetails{constructor(){this.userDetails={}}render(){return console.log("selected: user",this.userDetails),this.userDetails.name?o.html`
 				<strong>Person Details</strong>
 				<div>Name: ${this.userDetails.name}</div>
 				<div>Company: ${this.userDetails.company.name}</div>
-			`:o.html`<div></div>`}};Object(r.__decorate)([Object(o.Input)(),Object(r.__metadata)("design:type",Object)],a.prototype,"userDetails",void 0),a=Object(r.__decorate)([Object(o.Component)({selector:"person-details"})],a)}}]);
+			`:o.html`<div></div>`}};Object(r.__decorate)([Object(o.Input)(),Object(r.__metadata)("design:type",Object)],n.prototype,"userDetails",void 0),n=Object(r.__decorate)([Object(o.Component)({selector:"person-details"})],n)}}]);
--- docs/4.chunk.js
+++ docs/4.chunk.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{45:function(e,t,o){"use strict";o.r(t);var n=o(0),l=o(1),s=o(8);let a=class NestedModal{constructor(e){this.modalsrvc=e,this.nestedModalData={}}openAnotherModal(){const e=this.modalsrvc.show({renderTemplate:()=>l.html`<div>i'm nested modal</div>`,modalTitle:"nested modal",modalClass:"nested-class"});e.onOpen.subscribe(()=>{console.log("nested modal open")}),e.onClose.subscribe(()=>{console.log("nested modal closed")})}render(){return l.html`
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{45:function(e,t,o){"use strict";o.r(t);var l=o(0),n=o(1),s=o(8);let i=class NestedModal{constructor(e){this.modalsrvc=e,this.nestedModalData={}}openAnotherModal(){const e=this.modalsrvc.show({renderTemplate:()=>n.html`<div>i'm nested modal</div>`,modalTitle:"nested modal",modalClass:"nested-class"});e.onOpen.subscribe(()=>{console.log("nested modal open")}),e.onClose.subscribe(()=>{console.log("nested modal closed")})}render(){return n.html`
 			<div>sample modal</div>
 			<div>${this.nestedModalData.message}</div>
 			<button
@@ -7,31 +7,36 @@
 			>
 				open another modal
 			</button>
-		`}};Object(n.__decorate)([Object(l.Input)(),Object(n.__metadata)("design:type",Object)],a.prototype,"nestedModalData",void 0),a=Object(n.__decorate)([Object(l.Component)({selector:"nested-modal"}),Object(n.__metadata)("design:paramtypes",[s.ModalService])],a);let i=class PlumeComponents{constructor(e,t){this.modalsrvc=e,this.notifySrvc=t,this.toggleInput={onchange:this.onToggleChange.bind(this),onText:"ON",offText:"OFF"},this.multiSelectOptions={data:[{name:"option1"},{name:"option2"},{name:"option3"},{name:"option4"},{name:"option5"}],displayField:"name",multiple:!1,enableFilter:!0,disableDropdown:!1,buttonText:e=>0===e.length?"None selected":e.length>3?e.length+" selected":e.map(e=>e.name).join(", "),onchange:e=>{console.log(e)}}}openModal(){const e=this.modalsrvc.show({renderTemplate:()=>l.html`<nested-modal nestedModalData=${{message:"Hello World"}}></nested-modal>`,modalTitle:"testing modal",modalClass:"sample-class"});e.onOpen.subscribe(()=>{console.log("main modal open",e.Id)}),e.onClose.subscribe(()=>{console.log("main modal closed")})}notify(){this.notifySrvc.sendMessage("hello world",s.NotificationType.Info)}onToggleChange(e){console.log(e)}render(){return l.html`
+		`}};Object(l.__decorate)([Object(n.Input)(),Object(l.__metadata)("design:type",Object)],i.prototype,"nestedModalData",void 0),i=Object(l.__decorate)([Object(n.Component)({selector:"nested-modal"}),Object(l.__metadata)("design:paramtypes",[s.ModalService])],i);let a=class PlumeComponents{constructor(e,t){this.modalsrvc=e,this.notifySrvc=t,this.toggleInput={onchange:this.onToggleChange.bind(this),onText:"ON",offText:"OFF"},this.toggleMultiSelect={onchange:e=>{this.multiSelectOptions.multiple=e,this.multiSelectOptions.resetWidget=!0,this.update()}},this.multiSelectOptions={data:[{name:"option1"},{name:"option2"},{name:"option3"},{name:"option4"},{name:"option5"}],displayField:"name",multiple:!1,enableFilter:!0,disableDropdown:!1,buttonText:e=>0===e.length?"None selected":e.length>3?e.length+" selected":e.map(e=>e.name).join(", "),onchange:e=>{console.log(e)}}}openModal(){const e=this.modalsrvc.show({renderTemplate:()=>n.html`<nested-modal nestedModalData=${{message:"Hello World"}}></nested-modal>`,modalTitle:"testing modal",modalClass:"sample-class"});e.onOpen.subscribe(()=>{console.log("main modal open",e.Id)}),e.onClose.subscribe(()=>{console.log("main modal closed")})}notify(){this.notifySrvc.sendMessage("hello world",s.NotificationType.Info)}onToggleChange(e){console.log(e)}render(){return n.html`
             <div>
-				<h2 class='mb-20'>Plumejs UI Control Collection</h2>
-				<div class='mb-20'>
-					<h5>Modal</h5>
-					<button
-						class="btn btn-sm btn-primary"
-						onclick=${()=>{this.openModal()}}
-					>
-						Open Modal
-					</button>
-				</div>
-				<div class='mb-20'>
-					<h5>Notification</h5>
-					<button class='btn btn-sm btn-primary' onclick=${()=>{this.notify()}}>Notify</button>
-				</div>
-				<div class='mb-20'>
-					<h5>Toggle Button</h5>
-					<toggle-button toggleOptions=${this.toggleInput}></toggle-button>
-				</div>
-				<div class='mb-20'>
-					<h5>Multi select</h5>
-					<div style='width: 300px'>
-						<multi-select multiSelectOptions=${this.multiSelectOptions}></multi-select>
-					</div>
-				</div>
+							<h2 class='mb-20'>Plumejs UI Control Collection</h2>
+							<div class='mb-20'>
+								<h5>Modal</h5>
+								<button
+									class="btn btn-sm btn-primary"
+									onclick=${()=>{this.openModal()}}
+								>
+									Open Modal
+								</button>
+							</div>
+							<div class='mb-20'>
+								<h5>Notification</h5>
+								<button class='btn btn-sm btn-primary' onclick=${()=>{this.notify()}}>Notify</button>
+							</div>
+							<div class='mb-20'>
+								<h5>Toggle Button</h5>
+								<toggle-button toggleOptions=${this.toggleInput}></toggle-button>
+							</div>
+							<div class='mb-20'>
+								<h5>Multi select</h5>
+								<div style='width: 500px'>
+									<div class='d-flex mb-20'>
+										<span>enable multi select</span> <toggle-button toggleOptions=${this.toggleMultiSelect}></toggle-button>
+									</div>
+								</div>
+								<div style='width: 300px'>
+									<multi-select multiSelectOptions=${this.multiSelectOptions}></multi-select>
+								</div>
+							</div>
             </div>
-        `}};i=Object(n.__decorate)([Object(l.Component)({selector:"plume-comp"}),Object(n.__metadata)("design:paramtypes",[s.ModalService,s.NotificationService])],i)}}]);
+        `}};a=Object(l.__decorate)([Object(n.Component)({selector:"plume-comp"}),Object(l.__metadata)("design:paramtypes",[s.ModalService,s.NotificationService])],a)}}]);
