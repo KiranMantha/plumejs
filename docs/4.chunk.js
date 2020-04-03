32bcefcb61ec490830bcc87eb45ba8a8806d53bd
KiranMantha <murali-evoke@gmail.com>
2020-04-03 17:11:10 +0530

    updated demo files

--- docs/4.chunk.js
+++ docs/4.chunk.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{45:function(e,t,o){"use strict";o.r(t);var l=o(0),s=o(1),n=o(8);let i=class NestedModal{constructor(e){this.modalsrvc=e,this.nestedModalData={}}openAnotherModal(){const e=this.modalsrvc.show({renderTemplate:()=>s.html`<div>i'm nested modal</div>`,modalTitle:"nested modal",modalClass:"nested-class"});e.onOpen.subscribe(()=>{console.log("nested modal open")}),e.onClose.subscribe(()=>{console.log("nested modal closed")})}render(){return s.html`
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{45:function(e,t,o){"use strict";o.r(t);var l=o(0),s=o(1),i=o(8);let n=class NestedModal{constructor(e){this.modalsrvc=e,this.nestedModalData={}}openAnotherModal(){const e=this.modalsrvc.show({renderTemplate:()=>s.html`<div>i'm nested modal</div>`,modalTitle:"nested modal",modalClass:"nested-class"});e.onOpen.subscribe(()=>{console.log("nested modal open")}),e.onClose.subscribe(()=>{console.log("nested modal closed")})}render(){return s.html`
 			<div>sample modal</div>
 			<div>${this.nestedModalData.message}</div>
 			<button
@@ -7,34 +7,37 @@
 			>
 				open another modal
 			</button>
-		`}};Object(l.__decorate)([Object(s.Input)(),Object(l.__metadata)("design:type",Object)],i.prototype,"nestedModalData",void 0),i=Object(l.__decorate)([Object(s.Component)({selector:"nested-modal"}),Object(l.__metadata)("design:paramtypes",[n.ModalService])],i);let a=class PlumeComponents{constructor(e,t){this.modalsrvc=e,this.notifySrvc=t,this.toggleInput={onchange:this.onToggleChange.bind(this),onText:"ON",offText:"OFF"},this.multiselectToggles={enableMultiselect:{onchange:e=>{this.multiSelectOptions.multiple=e,this.multiSelectOptions.resetWidget=!0,this.update()}},disableDropdown:{onchange:e=>{this.multiSelectOptions.disableDropdown=e,this.update()}}},this.multiSelectOptions={data:[{name:"option1"},{name:"option2"},{name:"option3"},{name:"option4"},{name:"option5"}],displayField:"name",multiple:!1,enableFilter:!0,disableDropdown:!1,buttonText:e=>0===e.length?"None selected":e.length>3?e.length+" selected":e.map(e=>e.name).join(", "),onchange:e=>{console.log(e)}}}openModal(){const e=this.modalsrvc.show({renderTemplate:()=>s.html`<nested-modal nestedModalData=${{message:"Hello World"}}></nested-modal>`,modalTitle:"testing modal",modalClass:"sample-class"});e.onOpen.subscribe(()=>{console.log("main modal open",e.Id)}),e.onClose.subscribe(()=>{console.log("main modal closed")})}notify(){this.notifySrvc.sendMessage("hello world",n.NotificationType.Info)}onToggleChange(e){console.log(e)}render(){return s.html`
+		`}};Object(l.__decorate)([Object(s.Input)(),Object(l.__metadata)("design:type",Object)],n.prototype,"nestedModalData",void 0),n=Object(l.__decorate)([Object(s.Component)({selector:"nested-modal"}),Object(l.__metadata)("design:paramtypes",[i.ModalService])],n);let a=class PlumeComponents{constructor(e,t){this.modalsrvc=e,this.notifySrvc=t,this.toggleInput={onchange:this.onToggleChange.bind(this),onText:"ON",offText:"OFF"},this.multiselectToggles={enableMultiselect:{onchange:e=>{this.multiSelectOptions.multiple=e,this.multiSelectOptions.resetWidget=!0,this.update()}},disableDropdown:{onchange:e=>{this.multiSelectOptions.disableDropdown=e,this.update()}},enableFilter:{onchange:e=>{this.multiSelectOptions.enableFilter=e,this.update()}}},this.multiSelectOptions={data:[{name:"option1"},{name:"option2"},{name:"option3"},{name:"option4"},{name:"option5"}],displayField:"name",multiple:!1,disableDropdown:!1,buttonText:e=>0===e.length?"None selected":e.length>3?e.length+" selected":e.map(e=>e.name).join(", "),onchange:e=>{console.log(e)}}}openModal(){const e=this.modalsrvc.show({renderTemplate:()=>s.html`<nested-modal nestedModalData=${{message:"Hello World"}}></nested-modal>`,modalTitle:"testing modal",modalClass:"sample-class"});e.onOpen.subscribe(()=>{console.log("main modal open",e.Id)}),e.onClose.subscribe(()=>{console.log("main modal closed")})}notify(){this.notifySrvc.sendMessage("hello world",i.NotificationType.Info)}onToggleChange(e){console.log(e)}render(){return s.html`
 					<div>
-						<h2 class='mb-20'>Plumejs UI Control Collection</h2>
+						<h2 class='title is-3 mb-20'>Plumejs UI Control Collection</h2>
 						<div class='mb-20'>
-							<h5>Modal</h5>
+							<h5 class='title is-5'>Modal</h5>
 							<button
-								class="button is-small is-info"
+								class="button is-info"
 								onclick=${()=>{this.openModal()}}
 							>
 								Open Modal
 							</button>
 						</div>
 						<div class='mb-20'>
-							<h5>Notification</h5>
-							<button class='button is-small is-info' onclick=${()=>{this.notify()}}>Notify</button>
+							<h5 class='title is-5'>Notification</h5>
+							<button class='button is-info' onclick=${()=>{this.notify()}}>Notify</button>
 						</div>
 						<div class='mb-20'>
-							<h5>Toggle Button</h5>
+							<h5 class='title is-5'>Toggle Button</h5>
 							<toggle-button toggleOptions=${this.toggleInput}></toggle-button>
 						</div>
 						<div class='mb-20'>
-							<h5>Multi select</h5>
+							<h5 class='title is-5'>Multi select</h5>
 							<div style='width: 500px'>
-								<div class='d-flex mb-20'>
+								<div class='is-flex mb-20'>
 									<span>enable multi select</span> <toggle-button toggleOptions=${this.multiselectToggles.enableMultiselect}></toggle-button>
 								</div>
-								<div class='d-flex mb-20'>
+								<div class='is-flex mb-20'>
 									<span>disable dropdown</span> <toggle-button toggleOptions=${this.multiselectToggles.disableDropdown}></toggle-button>
+								</div>
+								<div class='is-flex mb-20'>
+									<span>enable filtering</span> <toggle-button toggleOptions=${this.multiselectToggles.enableFilter}></toggle-button>
 								</div>
 							</div>
 							<div style='width: 300px'>
@@ -42,4 +45,4 @@
 							</div>
 						</div>
 					</div>
-			`}};a=Object(l.__decorate)([Object(s.Component)({selector:"plume-comp"}),Object(l.__metadata)("design:paramtypes",[n.ModalService,n.NotificationService])],a)}}]);
+			`}};a=Object(l.__decorate)([Object(s.Component)({selector:"plume-comp"}),Object(l.__metadata)("design:paramtypes",[i.ModalService,i.NotificationService])],a)}}]);
--- docs/5.chunk.js
+++ docs/5.chunk.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{42:function(t,e,s){"use strict";s.r(e);var o=s(0),n=s(1);let i=class SampleService{constructor(){}testMeth(){console.log("testmethod in sample service")}};i=Object(o.__decorate)([Object(n.Injectable)(),Object(o.__metadata)("design:paramtypes",[])],i);let c=class TestService{constructor(t){this.sampleSrvc=t}testMeth(){this.sampleSrvc.testMeth()}getUsers(){return fetch("https://api.github.com/users?since=135")}};c=Object(o.__decorate)([Object(n.Injectable)(),Object(o.__metadata)("design:paramtypes",[i])],c);let l=class TestEle{constructor(){this.testprops={}}render(){return n.html`
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{42:function(t,e,s){"use strict";s.r(e);var o=s(0),n=s(1);let c=class SampleService{constructor(){}testMeth(){console.log("testmethod in sample service")}};c=Object(o.__decorate)([Object(n.Injectable)(),Object(o.__metadata)("design:paramtypes",[])],c);let i=class TestService{constructor(t){this.sampleSrvc=t}testMeth(){this.sampleSrvc.testMeth()}getUsers(){return fetch("https://api.github.com/users?since=135")}};i=Object(o.__decorate)([Object(n.Injectable)(),Object(o.__metadata)("design:paramtypes",[c])],i);let l=class TestEle{constructor(){this.testprops={}}render(){return n.html`
 			<div>
 				testing web component2 ${this.testprops.name}
 				<button class='button is-small is-info' onclick=${t=>this.counts(t)}>hi</button>
@@ -10,14 +10,14 @@
 		`}counts(t){this.testprops.oncount("testing from click")}change(t){this.testprops.oncount(t)}mount(){console.log("component loaded"),console.log("props: ",this.testprops)}unmount(){console.log("component unloaded")}};Object(o.__decorate)([Object(n.Input)(),Object(o.__metadata)("design:type",Object)],l.prototype,"testprops",void 0),l=Object(o.__decorate)([Object(n.Component)({selector:"test-ele"})],l);let a=class SampleEle{constructor(t){this.testSrvc=t,this.inputField=Object(n.useRef)(null),this.test="sample 123",this.outCount=this.count.bind(this),this.props={oncount:this.outCount,name:this.test}}enablePersonsRoute(){window.localStorage.setItem("plumejs","now persons route is activated")}disablePersonsRoute(){window.localStorage.removeItem("plumejs")}render(){return n.html`
 			<p>Persons route has <b>canActivate</b> gaurd which check for <i>plumejs</i> key in localstorage. Click enable button to navigate to persons route. Click disable button to disable persons route. </p>
 			<div>
-				<button class='button is-small is-info' onclick=${this.enablePersonsRoute} title='click persons nav to check persons route'>Enable Persons route</button>
-				<button class='button is-small is-info' style='margin-left: 10px' onclick=${this.disablePersonsRoute} title='click persons nav to check persons route'>Disable Persons route</button>
+				<button class='button is-info' onclick=${this.enablePersonsRoute} title='click persons nav to check persons route'>Enable Persons route</button>
+				<button class='button is-info' style='margin-left: 10px' onclick=${this.disablePersonsRoute} title='click persons nav to check persons route'>Disable Persons route</button>
 			</div>
-			<div>${"username.greet".translate({name:"test user"})}</div>
+			<div class='mt-20'>check translation: ${"username.greet".translate({name:"test user"})}</div>
 			<input type='text' ref=${this.inputField} /><button class='button is-small is-info' onclick=${()=>{this.getRef()}}>click</button>
 			<div>
 				<h1>Sample two way data binding</h1>
 				testing web component1 ${this.test}
 				<test-ele testprops=${this.props}></test-ele>
 			</div>
-		`}count(t){this.test=t,this.props.name=t,this.update()}beforeMount(){console.log("before mounting...")}mount(){console.log("component loaded"),console.log(this.inputField),this.testSrvc.testMeth()}unmount(){console.log("component unloaded")}getRef(){console.log(this.inputField)}};a=Object(o.__decorate)([Object(n.Component)({selector:"sample-ele"}),Object(o.__metadata)("design:paramtypes",[c])],a)}}]);
+		`}count(t){this.test=t,this.props.name=t,this.update()}beforeMount(){console.log("before mounting...")}mount(){console.log("component loaded"),console.log(this.inputField),this.testSrvc.testMeth()}unmount(){console.log("component unloaded")}getRef(){console.log(this.inputField)}};a=Object(o.__decorate)([Object(n.Component)({selector:"sample-ele"}),Object(o.__metadata)("design:paramtypes",[i])],a)}}]);
