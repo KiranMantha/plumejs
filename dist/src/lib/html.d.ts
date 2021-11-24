fd4f3628b98290781583deb58bd634dbf8959669
KiranMantha <kiranv.mantha@gmail.com>
2021-11-24 19:33:44 +0530

    made html engine as IIFE to prevent object pollution

--- dist/src/lib/html.d.ts
+++ dist/src/lib/html.d.ts
@@ -1,3 +1,2 @@
-declare const html: (templates: TemplateStringsArray, ...values: Array<any>) => DocumentFragment;
-declare const render: (where: HTMLElement, what: DocumentFragment) => void;
+declare const html: (templates: TemplateStringsArray, ...values: Array<any>) => DocumentFragment, render: (where: HTMLElement, what: DocumentFragment) => void;
 export { html, render };
