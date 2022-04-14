0858a3a7d24da9d8d5d80d18647b5e201b1aa4dd
KiranMantha <kiranv.mantha@gmail.com>
2022-04-14 16:10:07 +0530

    added modern animationsComplete method to detect animations completed on given element

--- dist/cjs/src/lib/domTransition.service.js
+++ dist/cjs/src/lib/domTransition.service.js
@@ -9,22 +9,6 @@ let DomTransition = class DomTransition {
         this.transition = '';
         this.whichTransitionEnd();
     }
-    whichTransitionEnd() {
-        const element = document.createElement('div');
-        const styleobj = element.style;
-        const transitions = {
-            transition: 'transitionend',
-            WebkitTransition: 'webkitTransitionEnd',
-            MozTransition: 'transitionend',
-            OTransition: 'otransitionend'
-        };
-        for (const t in transitions) {
-            if (typeof styleobj[t] !== 'undefined') {
-                this.transition = transitions[t];
-                break;
-            }
-        }
-    }
     onTransitionEnd(element, cb, duration) {
         let called = false;
         let unSubscribeEvent = null;
@@ -40,6 +24,30 @@ let DomTransition = class DomTransition {
             _fn();
         });
         setTimeout(_fn, duration);
+    }
+    animationsComplete(element) {
+        if (element.getAnimations) {
+            return Promise.allSettled(element.getAnimations().map((animation) => animation.finished));
+        }
+        else {
+            return Promise.allSettled([true]);
+        }
+    }
+    whichTransitionEnd() {
+        const element = document.createElement('div');
+        const styleobj = element.style;
+        const transitions = {
+            transition: 'transitionend',
+            WebkitTransition: 'webkitTransitionEnd',
+            MozTransition: 'transitionend',
+            OTransition: 'otransitionend'
+        };
+        for (const t in transitions) {
+            if (typeof styleobj[t] !== 'undefined') {
+                this.transition = transitions[t];
+                break;
+            }
+        }
     }
 };
 DomTransition = (0, tslib_1.__decorate)([
--- dist/src/lib/domTransition.service.d.ts
+++ dist/src/lib/domTransition.service.d.ts
@@ -1,6 +1,7 @@
 export declare class DomTransition {
     private transition;
     constructor();
-    private whichTransitionEnd;
     onTransitionEnd(element: HTMLElement, cb: () => void, duration: number): void;
+    animationsComplete(element: HTMLElement): Promise<any>;
+    private whichTransitionEnd;
 }
